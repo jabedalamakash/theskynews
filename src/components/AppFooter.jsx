e53edfe601ba7e5d08d1564ee65afd80a873aaff
jabedalamakash <jabedalamakash00@gmail.com>
2024-01-26 17:44:03 +0600

    fetch error fixed

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
 
-const socialData=async()=>{
-const fetcher= await fetch("http://localhost:3000/api/socials");
-const fetchData= await fetcher.json();
-return fetchData;
-}
+// const socialData=async()=>{
+// const fetcher= await fetch("http://localhost:3000/api/socials");
+// const fetchData= await fetcher.json();
+// return fetchData;
+// }
 
 export default async function AppFooter() {
-    const data=await socialData()
-    console.log(data)
+    // const data=await socialData()
+    // console.log(data)
   return (
     <div>
-      <p className='bg-blue-600 text-white'>copyright @2024 The Sky News</p>
+      <p className='bg-blue-600 text-center py-6 text-white'>Copyright @2024 The Sky News</p>
     </div>
   )
 }
